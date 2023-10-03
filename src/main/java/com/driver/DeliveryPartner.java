06f45d0e1e94ecfeb238187021661302ed7b344b
Shivaksk119 <shivapatelkarru119ksk@gmail.com>
2023-10-04 03:10:02 +0530

    1st Commit

--- src/main/java/com/driver/DeliveryPartner.java
+++ src/main/java/com/driver/DeliveryPartner.java
@@ -1,13 +1,22 @@
 package com.driver;
 
+import javax.print.attribute.standard.PrinterURI;
+import java.security.PrivateKey;
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class DeliveryPartner {
 
     private String id;
     private int numberOfOrders;
+    private boolean isOrderAssigned;
+    private ArrayList<String> AssignedOrdersList;
 
     public DeliveryPartner(String id) {
         this.id = id;
         this.numberOfOrders = 0;
+        this.isOrderAssigned = false;
+        this.AssignedOrdersList = new ArrayList<>();
     }
 
     public String getId() {
@@ -15,10 +24,27 @@ public class DeliveryPartner {
     }
 
     public Integer getNumberOfOrders(){
+
         return numberOfOrders;
     }
 
     public void setNumberOfOrders(Integer numberOfOrders) {
         this.numberOfOrders = numberOfOrders;
+    }
+
+    public boolean isOrderAssigned() {
+        return isOrderAssigned;
+    }
+
+    public void setOrderAssigned(boolean orderAssigned) {
+        isOrderAssigned = orderAssigned;
+    }
+
+    public ArrayList<String> getAssignedOrdersList() {
+        return AssignedOrdersList;
+    }
+
+    public void setAssignedOrdersList(ArrayList<String> assignedOrdersList) {
+        AssignedOrdersList = assignedOrdersList;
     }
 }
